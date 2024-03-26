2919e596b23bfc4aafddb5c87b24289b70b9624a
ryoshindo <udoryo999@gmail.com>
2024-03-27 02:31:57 +0900

    feat: hello world

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -10,4 +10,10 @@ provider "fs" {
   name = "example"
 }
 
-data "fs_hello_world" "example" {}
+data "fs_hello_world" "example" {
+  name = "Ryo Shindo"
+}
+
+output "message" {
+  value = data.fs_hello_world.example.message
+}
--- internal/provider/hello_world_data_source.go
+++ internal/provider/hello_world_data_source.go
@@ -5,21 +5,57 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type helloWorldDataSource struct{}
+
+var (
+	_ datasource.DataSource              = &helloWorldDataSource{}
+	_ datasource.DataSourceWithConfigure = &helloWorldDataSource{}
 )
 
 func NewHelloWorldDataSource() datasource.DataSource {
 	return &helloWorldDataSource{}
 }
 
-type helloWorldDataSource struct{}
-
 func (d *helloWorldDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_hello_world"
 }
 
+// Schema defines the schema for the data source.
 func (d *helloWorldDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
-	resp.Schema = schema.Schema{}
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"name": schema.StringAttribute{
+				Required: true,
+			},
+			"message": schema.StringAttribute{
+				Computed: true,
+			},
+		},
+	}
 }
 
 func (d *helloWorldDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var config helloWorldDataSourceModel
+
+	diags := req.Config.Get(ctx, &config)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	config.Message = types.StringValue("Hello, " + config.Name.ValueString() + "!")
+
+	diags = resp.State.Set(ctx, config)
+	resp.Diagnostics.Append(diags...)
+}
+
+func (d *helloWorldDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+}
+
+type helloWorldDataSourceModel struct {
+	Name    types.String `tfsdk:"name"`
+	Message types.String `tfsdk:"message"`
 }
